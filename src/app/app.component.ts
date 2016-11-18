c7339f843c0645653bec493d734032bfccc72aa8
Marius Oprea <oprea_marius@yahoo.com>
2016-11-18 04:21:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $ : any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent extends OnInit
+{
+  ngOnInit()
+  {	  	
+	<any>$.material.init();
+	<any>$.material.ripples();
+	<any>$.material.input();
+	<any>$.material.checkbox();
+	<any>$.material.radio();
+  }
 }
